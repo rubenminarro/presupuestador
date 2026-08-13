f7baacf539c2a9da157c6918a2e83c6c0b500548
Ruben Miñarro <ruben.minarro@grupocondor.com.py>
2026-08-12 23:34:44 -0300

    Diagnostic items, seeders, controllers, migrations y models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             CheckListItemServiceCategorySeeder::class,
             ReceptionSeeder::class,
             DiagnosticSeeder::class,
+            DiagnosticItemSeeder::class,
         ]);
     }
 }
